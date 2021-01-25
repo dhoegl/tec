5a57d37f8990519b97601f6e8d43301c5320adf6
dhoegl <firebird@hoeglund.com>
2021-01-25 15:46:28 -0800

    Testing this file update in PC or GIT or BOTH

--- tec_welcome.php
+++ tec_welcome.php
@@ -5,6 +5,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--TESTING WHERE THIS FILE IS STORED-->
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
