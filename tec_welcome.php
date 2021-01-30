c1e669e5d88b908f5865f9e52cff0c79ed9a36dc
dhoegl <firebird@hoeglund.com>
2021-01-30 11:17:15 -0800

    Ibid

--- tec_welcome.php
+++ tec_welcome.php
@@ -126,7 +126,7 @@ session_start();
                                 <h6 class="text-center white-text">
                                     If you forgot your password, click on the FORGOT PASSWORD button above to be sent a temporary password to your email address on file
                                 </h6>
-                                <p class="fs-6">
+                                <p class="fs-6 text-start white-text">
                                     (version tec_20210130.1)
                                 </p>
                             </div><!--btn-->
