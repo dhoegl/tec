dc25d6cfb35c746e2301d3dd60688247f413190d
dhoegl <firebird@hoeglund.com>
2021-01-30 14:32:07 -0800

    Ibid3

--- includes/tec_footer.php
+++ includes/tec_footer.php
@@ -2,17 +2,18 @@
 <!-- <footer class="page-footer fixed-bottom font-small blue"> -->
 <footer class="page-footer fixed-bottom font-small orange accent-4">
 <div class="col-sm-4">
+  <span> </span>
 </div>
   <!-- Copyright -->
   <div class="col-sm-4">
-    <div class="footer-copyright text-center py-3">© 2021 Copyright:
-      <a href="#"> OurFamilyConnections.org</a>
+    <div class="footer-copyright text-center py-3">© 2021 Copyright:<a href="//tec.ourfamilyconnections.org"> OurFamilyConnections.org</a>
+
     </div>
   </div>
   <!-- Copyright -->
   <!-- SSL cert seal -->
   <div class="col-sm-4">
-    <span id="siteseal"><class="float-end"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></span>
+    <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></span>
   </div>
   <!-- SSL cert seal -->
 
