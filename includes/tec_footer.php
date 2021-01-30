29fe34fc2274bcd6f1c60851095aa988058c61aa
dhoegl <firebird@hoeglund.com>
2021-01-30 14:21:33 -0800

    Ibid

--- includes/tec_footer.php
+++ includes/tec_footer.php
@@ -5,7 +5,7 @@
   <!-- Copyright -->
   <div class="footer-copyright text-center py-3">© 2021 Copyright:
     <a href="#"> OurFamilyConnections.org</a>
-    <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></span>
+    <span id="siteseal"><class="float-end"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></span>
   </div>
   <!-- Copyright -->
 
