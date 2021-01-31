aad624ec69bec28834e4d47008211eda5a20340e
dhoegl <firebird@hoeglund.com>
2021-01-31 15:41:37 -0800

    Replaced js scripts at end of profile (using NEW)

--- tec_profile.php
+++ tec_profile.php
@@ -2133,12 +2133,21 @@ var jQ55 = jQuery.noConflict();
   <!-- SCRIPTS -->
 <!-- JQuery -->
 <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/MDBootstrap4191/popper.min.js"></script>
+
+<!-- NEW Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
+
+  <!-- OLD Bootstrap tooltips -->
+  <!-- <script type="text/javascript" src="js/MDBootstrap4191/popper.min.js"></script> -->
   <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/MDBootstrap4191/bootstrap.min.js"></script>
+  <!-- <script type="text/javascript" src="js/MDBootstrap4191/bootstrap.min.js"></script> -->
   <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/MDBootstrap4191/mdb.min.js"></script>
+  <!-- <script type="text/javascript" src="js/MDBootstrap4191/mdb.min.js"></script> -->
   <!-- Tenant Configuration JavaScript Call in tec_nav -->
   <!-- Datatables JavaScript plugins - Bootstrap-specific -->
     <!-- Works kind of -->
