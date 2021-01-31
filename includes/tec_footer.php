31b71cc89d988d2388a6c4c76fcbcc74c814d4ce
dhoegl <firebird@hoeglund.com>
2021-01-30 22:29:56 -0800

    Ibid3

--- includes/tec_footer.php
+++ includes/tec_footer.php
@@ -2,19 +2,18 @@
 <footer class="page-footer fixed-bottom font-small orange accent-4">
     <!-- Grid row -->
     <div class="row d-flex align-items-center">
-    <!-- Grid column -->
-    <div class="col-md-7 col-lg-8">
-      <!--Copyright-->
-      <p class="text-center text-md-left">© 2021 Copyright:
-        <a href="//tec.ourfamilyconnections.org"><strong> OurFamilyConnections.org</strong></a>
-      </p>
-    </div>
-    <!-- Grid column -->
-    <div class="col-md-5 col-lg-4 ml-lg-0">
-  <!-- SSL cert seal -->
-      <p id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></p>
-    </div>
-  <!-- SSL cert seal -->
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2021 Copyright:
+          <a href="//tec.ourfamilyconnections.org"><strong> OurFamilyConnections.org</strong></a>
+        </p>
+      </div>
+      <!-- Grid column -->
+      <!-- <div class="col-md-5 col-lg-4 ml-lg-0">
+        <p id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></p>
+      </div> -->
+      <!-- SSL cert seal -->
   </div>
 
 </footer>
