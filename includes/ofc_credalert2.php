8eae06743560f58d554beea5197437ce37912153
dhoegl <firebird@hoeglund.com>
2021-02-01 22:48:07 -0800

    Removed 2 ofc files

--- includes/ofc_credalert2.php
+++
@@ -1,9 +0,0 @@
-<script type='text/javascript'>
- var jQ2 = jQuery.noConflict();
-	jQ2(document).ready(function() {
-		alert("Sorry!!\n\nYour membership has not yet been activated.\nOur Administrators have received your request.\n\n Thank you for your patience!");
-		console.log("User not yet activated");
-		window.location.replace("//tec.ourfamilyconnections.org/tec_welcome.php");
-
-});
-</script>
