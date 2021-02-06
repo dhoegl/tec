f4027f13199e5a24f228f0f1dfd6bcf863c6a1ff
dhoegl <firebird@hoeglund.com>
2021-02-06 00:02:01 -0800

    Ibid4

--- tec_help.php
+++ tec_help.php
@@ -77,7 +77,17 @@ $count = $result->num_rows;
     ?>
     <!-- Intro Section -->
     <div class="container-fluid profile_bg bottom-buffer">
-    <div class="row">
+    <div class="row pt-2">
+            <div class="col-sm-12">
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    Using the Registration Admin page
+                </button>
+                <!--<button class="btn btn-danger" type="button" data-toggle="collapse" data-target="#testalert" aria-expanded="false" aria-controls="testalert">
+                Test Alert
+            </button>-->
+            </div><!-- col sm-12 -->
+        </div><!-- row -->
+         <div class="row">
             <div class="col-sm-12">
                 <div class="card card-image"
                     style="background-image: url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg);">
