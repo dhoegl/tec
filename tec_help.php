f9b4d28dc04e5563eb8bee5746a7aa608404d27d
dhoegl <firebird@hoeglund.com>
2021-02-06 21:23:58 -0800

    Ibid

--- tec_help.php
+++ tec_help.php
@@ -108,13 +108,15 @@ require_once('tec_dbconnect.php');
 					            <div class="faq_question">Where do I go to download a printable directory?</div>
 						        <div class="faq_answer_container">
 							        <div class="faq_answer">Go to the <a href="tec_family.php">Directory</a> page and click on the blue button at the top-left of the page. You will find both a PDF version of the directory and a CSV version to copy into your favorite spreadheet.</div>
-						        </div>		
+                                </div>
+                                <br />		
 				            </div>
 				            <div class="faq">
 					            <div class="faq_question">Is there a way to print out the calendar?</div>
 						        <div class="faq_answer_container">
 							        <div class="faq_answer">Not at this time - stay tuned!</div>
-						        </div>		
+                                </div>
+                                <br />		
 				            </div>
 				            <div class="faq">
 					            <div class="faq_question">Who do I contact if I have questions or need more details on how to use this site?</div>
