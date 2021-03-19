ad6908b0de7e62295c2e34eece47272a4a88c3c8
dhoegl <firebird@hoeglund.com>
2021-03-19 10:01:37 -0700

    Mailtest

---
+++ services/mailtest.php
@@ -0,0 +1,15 @@
+<?php
+    $regmailsubject = "TEST - Registration Request to TEC Family Connections"."\n..";
+    $regmailmessage = "<html><body>";
+    $regmailmessage .= "<p style='background-color: #ff6933; font-size: 30px; font-weight: bold; color: white; padding: 25px; width=100%;'> Trinity Evangel Church</p>";
+    $regmailmessage .= "<p>Hello! </p>";
+    $regmailmessage .= "<p><strong>" . "DanTEST" . " " . "HoeglundTEST" . "</strong>";
+    $regmailmessage .= " has requested to be added to Trinity Evangel Church's Directory site.</p>";
+    $regmailmessage .= "<p>Login to our site using your admin credentials, select the <strong>Registration Admin</strong> menu item, and accept or reject this request " . "<a href='" . $regmaillink . "'>" . $regmaillink . "</a></p>";
+    $regmailmessage .= "</body></html>";
+    $regmailfrom = "newfamilyrequest@ourfamilyconnections.org";
+    $regmailheaders = "From:" . $regmailfrom . "\r\n";
+    $regmailheaders .= "MIME-Version: 1.0\r\n";
+    $regmailheaders .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    mail($regmailto,$regmailsubject,$regmailmessage,$regmailheaders);
+?>
