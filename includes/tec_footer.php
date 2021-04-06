aa4f8a722201d419b6a1940c50f2b1ef289373e9
dhoegl <firebird@hoeglund.com>
2021-04-05 19:55:08 -0700

    Ibid4

--- includes/tec_footer.php
+++ includes/tec_footer.php
@@ -22,30 +22,6 @@
         <p id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></p>
       </div> -->
       <!-- SSL cert seal -->
-  </div>
-
-</footer>
-<footer class="fixed-bottom font-small" id="footercolor">
-    <!-- Grid row -->
-    <div class="row">
-      <!-- Grid column -->
-      <div class="col-4">
-        <p class="text-center"></p>
-      </div>
-      <div class="col-4">
-        <!--Copyright-->
-        <p class="text-center" style="font-weight:bold;">Copyright © 2021 OurFamilyConnections.org</p>
-      </div>
-      <div class="col-4">
-        <!-- <p class="text-center">Privacy Policy</p> -->
-        <p class="text-center"><a href="../policies/Privacy.pdf" target="_blank" style="text-decoration:none; font-weight:bold;">Privacy Policy</a></p>
-        <!-- <a href="https://www.w3schools.com/" target="_blank">Visit W3Schools!</a> -->
-      </div>
-      <!-- Grid column -->
-      <!-- <div class="col-md-5 col-lg-4 ml-lg-0">
-        <p id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=m7LAm05LifE6FmuidmfjMBU1lY2wsrrIz4Q2Jd2S94MfOMixLrkty4NOjV5Y"></script></p>
-      </div> -->
-      <!-- SSL cert seal -->
-  </div>
+    </div>
 
 </footer>
