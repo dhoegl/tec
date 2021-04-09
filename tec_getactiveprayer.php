6a435c0c9280604bf596075e9fb527a41174c6d2
dhoegl <firebird@hoeglund.com>
2021-04-08 20:19:42 -0700

    Ibid4

--- tec_getactiveprayer.php
+++ tec_getactiveprayer.php
@@ -28,7 +28,7 @@ if(!$_SESSION['logged in']) {
 				$prayer_title = $activerow['prayertitle'];
 				$prayer_text = $activerow['prayertext'];
 				$fullname = $activerow['fullname'];				
-				$glance = "<strong>" . $prayer_title . " </strong><br />" . substr($activerow['prayertext'],0,50) . "...";
+				$glance = "<b>" . $prayer_title . " </b><br />" . substr($activerow['prayertext'],0,50) . "...";
 				$praypraise = $activerow['praypraise'];
 				$prayanswer = $activerow['answered'];
 				if($activerow['prayanswer'] == '1') {
