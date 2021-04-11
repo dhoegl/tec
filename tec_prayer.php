297f67f1f22bf47c712d0ae77499462cf8566b93
dhoegl <firebird@hoeglund.com>
2021-04-11 15:03:59 -0700

    Ibid4

--- tec_prayer.php
+++ tec_prayer.php
@@ -259,11 +259,21 @@ require_once('includes/tec_footer.php');
 <!-- Intro Section -->
     <div class="container-fluid profile_bg bottom-buffer">
         <div class="row pt-2">
-            <div class="col-sm-6">
+            <div class="col-sm-4">
                 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                     How to...
                 </button>
-            </div><!-- col sm-6 -->
+            </div><!-- col sm-4 -->
+			<div class="col-sm-4">
+					<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseNewPrayer" aria-expanded="false" aria-controls="collapseNewPrayer">
+						New Request
+					</button>
+				</div><!-- col sm-4 -->
+				<div class="col-sm-4">
+					<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseNewPrayer" aria-expanded="false" aria-controls="collapseNewPrayer">
+						Edit Request
+					</button>
+				</div><!-- col sm-4 -->
         </div><!-- row -->
         <div class="collapse" id="collapseExample">
             <div class="row">
@@ -307,18 +317,6 @@ require_once('includes/tec_footer.php');
                 </div><!-- col-sm-6 -->
             </div><!-- row -->
         </div><!-- collapse -->
-			<div class="row">
-				<div class="col-sm-6">
-					<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseNewPrayer" aria-expanded="false" aria-controls="collapseNewPrayer">
-						New Prayer Request
-					</button>
-				</div><!-- col sm-6 -->
-				<div class="col-sm-6">
-					<button class="btn btn-success" type="button" data-toggle="collapse" data-target="#collapseMyPrayer" aria-expanded="false" aria-controls="collapseMyPrayer">
-					My Prayer Requests
-					</button>
-				</div><!-- col sm-6 -->
-			</div><!-- row -->
             <div class="row">
                 <div class="col-sm-12">
                     <div class="card card-image"
