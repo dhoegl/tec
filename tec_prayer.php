2a360898ae0c133c12396d56832396110a2d796e
dhoegl <firebird@hoeglund.com>
2021-04-11 15:13:12 -0700

    Ibid6

--- tec_prayer.php
+++ tec_prayer.php
@@ -259,17 +259,17 @@ require_once('includes/tec_footer.php');
 <!-- Intro Section -->
     <div class="container-fluid profile_bg bottom-buffer">
         <div class="row pt-2">
-            <div class="col-sm-4">
+            <div class="col-xs-4">
                 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                     How to...
                 </button>
             </div><!-- col sm-4 -->
-			<div class="col-sm-4">
+			<div class="col-xs-4">
 					<button class="btn btn-success btn-sm" type="button" data-toggle="collapse" data-target="#collapseNewPrayer" aria-expanded="false" aria-controls="collapseNewPrayer">
 						New
 					</button>
 				</div><!-- col sm-4 -->
-				<div class="col-sm-4">
+				<div class="col-xs-4">
 					<button class="btn btn-success btn-sm" type="button" data-toggle="collapse" data-target="#collapseNewPrayer" aria-expanded="false" aria-controls="collapseNewPrayer">
 						Edit
 					</button>
