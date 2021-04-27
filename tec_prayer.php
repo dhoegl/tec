9fb99287261e40c4989f1aea5c605c6f5aa9721d
dhoegl <firebird@hoeglund.com>
2021-04-26 20:43:48 -0700

    Ibid6

--- tec_prayer.php
+++ tec_prayer.php
@@ -599,12 +599,12 @@ require_once('includes/tec_footer.php');
                                 <div class="col-8 text-left"><span id="prayerTitle"></span></div>
                             </div>
                             <div class="row">
-                                <div class="col-4 text-right font-weight-bold"><span class="border">Updated:</span></div>
-                                <div class="col-2 text-left border-bottom"><span id="prayerUpdate"></span></div>
-                                <div class="col-3 text-right font-weight-bold border-bottom">Answered:</div>
-                                <div class="col-3 text-left border-bottom"><span id="prayerAnswer"></span></div>
+                                <div class="col-4 text-right font-weight-bold">Updated:</div>
+                                <div class="col-2 text-left"><span id="prayerUpdate"></span></div>
+                                <div class="col-3 text-right font-weight-bold">Answered:</div>
+                                <div class="col-3 text-left"><span id="prayerAnswer"></span></div>
                             </div>
-                            <table id="viewpraytable" border='0' cellpadding='0' cellspacing='1' >
+                            <table id="viewpraytable" style="border: 1px solid black" cellpadding='0' cellspacing='1' >
 								<tbody>
 									<tr>
 										<td>Type:</td>
