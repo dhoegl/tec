e11deaa23125f97a8b6c15735bc53b2412714c9a
dhoegl <firebird@hoeglund.com>
2021-04-28 22:02:08 -0700

    Ibid5

--- tec_prayer.php
+++ tec_prayer.php
@@ -218,8 +218,10 @@ jQ9(document).ready(function () {
                 jQ9("#prayerType").html("Praise");
             }
             var prayerIndex = prayerID-1;
-            prayerText = testforChild.prev("table").find("#prayertexttable.full_text2").eq(prayerIndex).text();
-            prayerText2 = testforChild.prev("table").find("#prayertexttable.full_text2").text();
+            // prayerText = testforChild.parentElement.parentElement.find("#prayertexttable.full_text2").eq(prayerIndex).text();
+            prayerText = testforChild.parentElement.parentElement.nodeName;
+            // prayerText2 = testforChild.prev("table").find("#prayertexttable.full_text2").text();
+            prayerText2 = testforChild.parentElement.parentElement.find("#prayertexttable.full_text2").text();
             // jQ9("#prayerText").html(prayerText);
             console.log("prayerDate (this jQ9 entry) = " + prayerDate);
             console.log("prayerAnswer (this jQ9 entry) = " + prayerAnswer);
