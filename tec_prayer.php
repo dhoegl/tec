299e90af300d2a9f469314e2d3e36417bab8d34f
dhoegl <firebird@hoeglund.com>
2021-04-29 09:52:19 -0700

    Ibid2

--- tec_prayer.php
+++ tec_prayer.php
@@ -225,7 +225,8 @@ jQ9(document).ready(function () {
             if(parentTable.hasClass("dtr-inline")) {
                 console.log("**** I found the dtr-inline parent ****");
             }
-            siblingTable = jQ9(parentTable).find("#prayertexttable.full_text2").text();
+            // siblingTable = jQ9(parentTable).find("#prayertexttable.full_text2").text();
+            siblingTable = jQ9(parentTable).siblings("table").find(".full_text2").text();
             // siblingTable = jQ9(parentTable).find("#prayertexttable").find(".full_text2").eq(prayerIndex).css({"color": "red"});
             jQ9(parentTable).find("#prayertexttable").css({"color": "red"});
             // prayerText2 = testforChild.prev("table").find("#prayertexttable.full_text2").text();
