17e178e7efc8b5da24c7523592afb4dec63a0a44
dhoegl <firebird@hoeglund.com>
2021-04-29 17:37:32 -0700

    Ibid5

--- tec_prayer.php
+++ tec_prayer.php
@@ -668,10 +668,10 @@ require_once('includes/tec_footer.php');
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6 d-flex justify-content-center">
-                                        <button type="submit" name="submitnewprayer" class="btn btn-primary">Close</button>
+                                        <button type="submit" name="submitnewprayer" class="btn btn-primary btn-sm">Close</button>
                                     </div>
                                     <div class="col-sm-6 d-flex justify-content-center">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Follow</button>
+                                        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Follow</button>
                                     </div>
                                 </div>
                             </div>
