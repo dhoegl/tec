f26d12684071dc6092892c0866a3866352d9fc02
dhoegl <firebird@hoeglund.com>
2021-05-02 22:24:28 -0700

    Ibid3

--- services/tec_get_prayer_email_address.php
+++ services/tec_get_prayer_email_address.php
@@ -6,7 +6,7 @@ if(!$_SESSION['logged in']) {
 	session_destroy();
 	exit();
 }
-/* Get email address of selected prayer owner - called from tecprayer.php */
+/* Get email address of selected prayer owner - called from tec_prayer.php */
     require_once('../tec_dbconnect.php');
     
 	if ( !isset($_GET['prayerID'])) {
@@ -15,12 +15,13 @@ if(!$_SESSION['logged in']) {
 	}
 	else {
 		$prayerID = $_GET['prayerID'];
-		$prayerquery = "SELECT p.prayer_id, l.email_addr FROM " . $_SESSION['logintablename'] . " l INNER JOIN " . $_SESSION['prayertable'] . " p on p.name = CONCAT(l.firstname, ' ' , l.lastname) WHERE p.prayer_id = '" . $prayerID . "'";
+		// $prayerquery = "SELECT p.prayer_id, l.email_addr FROM " . $_SESSION['logintablename'] . " l INNER JOIN " . $_SESSION['prayertable'] . " p on p.name = CONCAT(l.firstname, ' ' , l.lastname) WHERE p.prayer_id = '" . $prayerID . "'";
+		$prayerquery = "SELECT p.prayer_id, l.email_addr FROM " . $_SESSION['logintablename'] . " l INNER JOIN " . $_SESSION['prayertable'] . " p on p.owner_id = l.login_ID WHERE p.prayer_id = '" . $prayerID . "'";
 		$prayerresult = $mysql->query($prayerquery) or die(" SQL query prayer follow table check error. Error #: " . $mysql->errno . " : " . $mysql->error);
 		$prayercount = $prayerresult->num_rows;
 		$prayerarray = array();
 
-		while($prayerrow = @mysql_fetch_assoc($prayerresult)) {
+		while($prayerrow = $prayerresult->fetch_assoc()) {
 			$prayerIDfromSelect = $prayerrow['prayer_id'];
 			$prayeremail = $prayerrow['email_addr'];
 			$buildjson = array('prayerid' => $prayerIDfromSelect, 'prayeremail' => $prayeremail);
