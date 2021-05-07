bbad1832f44e00fce062cbbe453a437a246e2da7
dhoegl <firebird@hoeglund.com>
2021-05-07 16:46:27 -0700

    Ibid12

--- tec_prayer.php
+++ tec_prayer.php
@@ -733,12 +733,13 @@ require_once('includes/tec_footer.php');
                         <div class="row">
                             <div class="col-12">
                                 <div class="row">
-                                    <div class="col-3">
-                                        <p class="mb-n1 text-left font-bold">WHO:</p>
-                                        <p class="mb-n1 text-left font-bold">WHEN:</p>
-                                        <p class="mb-n1 text-left font-bold">WHERE:</p>
+                                    <div class="col-2">
+                                        <p class="mb-n1 text-left font-weight-bold">Type:</p>
+                                        <p class="mb-n1 text-left font-weight-bold">Date:</p>
+                                        <p class="mb-n1 text-left font-weight-bold">From:</p>
+                                        <p class="mb-n1 text-left font-weight-bold">Title:</p>
                                     </div>
-                                    <div class="col-9">
+                                    <div class="col-10">
                                         <p class="mb-n1">Sed viverra ipsum nunc aliquet.</p>
                                         <p class="mb-n1">Sed viverra ipsum nunc aliquet. </p>
                                         <p class="mb-n1">Sed viverra ipsum nunc aliquet. </p>
