4c2d1a06f27d332859d8dc6804bcdc47f3a93dc9
dhoegl <firebird@hoeglund.com>
2021-05-10 22:10:31 -0700

    Ibid5

--- tec_prayer.php
+++ tec_prayer.php
@@ -90,7 +90,7 @@ if(!$_SESSION['logged in']) {
 		})
         masterprayer.done(function (masterresponse) {
                     //  Get the result
-                    var obj = masterresponse.responseText;
+                    var obj = masterprayer;
                     // teststat2 = response.responseText;
                     // var email = masterresponse[0].'0';
                     console.log('first entry = ' + obj);
