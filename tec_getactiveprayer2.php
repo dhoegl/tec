6012a9aedf4b73ce80725e882b85a66697839368
dhoegl <firebird@hoeglund.com>
2021-05-11 22:25:21 -0700

    Refresh active prayer list

--- tec_getactiveprayer2.php
+++ tec_getactiveprayer2.php
@@ -1,6 +1,7 @@
 <?php
 // ****************************** Get Actve Prayer Data **************************************
 // Called by tec_view_activeprayerlist2.php
+// Updated 20210511 - Altered SELECT query to OUTER JOIN to get ALL active prayer requests and not just those associated with logged in user
 session_start();
 if(!$_SESSION['logged in']) {
 	session_destroy();
@@ -11,7 +12,8 @@ if(!$_SESSION['logged in']) {
 // 	if (isset($_GET['action']) ) 
 //	{
 /*Query active prayer listing: visible = 3 (all) and status = 1 */
-		$activeprayerquery = "SELECT p.create_date AS prayerupdatedate, p.name AS fullname, m.Name_1 AS firsthim, m.Name_2 AS firsther, m.Surname AS last, p.prayer_id AS prayerid, p.title AS prayertitle, p.prayer_text AS prayertext, p.pray_praise AS praypraise, p.updated AS updatereq, p.answer AS prayanswer FROM " . $_SESSION['prayertable'] . " p INNER JOIN " . $_SESSION['dirtablename'] . " m on m.idDirectory = p.owner_id WHERE p.visible = '3' and p.status = '1' and p.approved='1' ORDER BY p.create_date DESC";
+		// $activeprayerquery = "SELECT p.create_date AS prayerupdatedate, p.name AS fullname, m.Name_1 AS firsthim, m.Name_2 AS firsther, m.Surname AS last, p.prayer_id AS prayerid, p.title AS prayertitle, p.prayer_text AS prayertext, p.pray_praise AS praypraise, p.updated AS updatereq, p.answer AS prayanswer FROM " . $_SESSION['prayertable'] . " p INNER JOIN " . $_SESSION['dirtablename'] . " m on m.idDirectory = p.owner_id WHERE p.visible = '3' and p.status = '1' and p.approved='1' ORDER BY p.create_date DESC";
+		$activeprayerquery = "SELECT p.create_date AS createdate, m.fullname AS full, m.firstname AS first, m.lastname as last, p.prayer_id AS prayerid, p.title AS prayertitle, p.prayer_text AS prayertext, p.pray_praise AS praypraise, p.updated AS updated, p.answer AS prayanswer FROM " . $_SESSION['prayertable'] . " p LEFT OUTER JOIN " . $_SESSION['logintablename'] . " m on m.login_ID = p.owner_id WHERE p.visible = '3' and p.status = '1' and p.approved='1' ORDER BY p.prayer_id ASC";
 		$activeprayerresult = $mysql->query($activeprayerquery) or die(" SQL query error at select active prayers. Error #: " . $mysql->errno . " : " . $mysql->error);
 		$activeprayercount = $activeprayerresult->num_rows;
 
@@ -29,10 +31,12 @@ if(!$_SESSION['logged in']) {
 		while($activerow = $activeprayerresult->fetch_assoc()) {
 				$activeprayercontrol = "<tr><td></td>";
 				$prayerid = "<td>" . $activerow['prayerid'] . "</td>";
-				$prayerupdate = "<td>" . date("M-d-Y", strtotime($activerow['prayerupdatedate'])) . "</td>";
-				$fullname = "<td>" . $activerow['fullname'] . "</td>";				
+				$prayerupdate = "<td>" . date("M-d-Y", strtotime($activerow['createdate'])) . "</td>";
+				$fullname = "<td>" . $activerow['full'] . "</td>";				
+				$firstname = "<td>" . $activerow['first'] . "</td>";
+				$lastname = "<td>" . $activerow['last'] . "</td>";
 				$praypraise = "<td>" . $activerow['praypraise'] . "</td>";
-				$prayanswer = $activerow['answered'];
+				$prayanswer = $activerow['prayanswer'];
 				if($activerow['prayanswer'] == '1') {
 					$prayanswer = "<td> YES </td>";
 				}
