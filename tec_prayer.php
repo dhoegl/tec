f362a9ca76078f883d8545f040a85399748f63d0
dhoegl <firebird@hoeglund.com>
2021-05-15 21:35:09 -0700

    My existing prayer request Details btn modal show

--- tec_prayer.php
+++ tec_prayer.php
@@ -441,7 +441,9 @@ jQ10(document).ready(function () {
             console.log("myprayerIndex (this jQ10 entry) = " + myprayerIndex);
             console.log("mysiblingTable (this jQ10 entry) = " + mysiblingTable);
 
-
+            // Display My Existing Prayer Request Details popup
+            jQ10("#ModalPrayerView").modal('hide');
+            jQ10("#ModalEditExistingRequest").modal('show');
 
         }
         else {
